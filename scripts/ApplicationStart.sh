2f56bfdc2c90b5918d6259ca1f14aab5a03e7f48
Fernando <70329877+fyonfa@users.noreply.github.com>
2020-11-27 11:46:58 +0000

    Update ApplicationStart.sh

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##### Start Service app
 
-cd /home/ubuntu/indoorSensor/
+#cd /home/ubuntu/indoorSensor/
 #####      Creating a service call indoorSensor
 #sudo forever-service install indoorSensor -r app
 ##### start a service called indoorSensor
